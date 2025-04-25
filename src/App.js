be88321c6eca8cc145f8bab910b48c08c182a0d4
richmen <52317216+richmen-antony@users.noreply.github.com>
2025-04-25 11:53:29 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Learn Amazone Web Service
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React js (richmen)
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
